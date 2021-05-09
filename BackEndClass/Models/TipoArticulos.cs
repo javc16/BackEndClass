3f3428c14ad9777cf47b4241bb192ace181be7f8
Danicboy <danicboy97@gmail.com>
2021-05-09 11:13:49 -0600

    Creacion de entidades TipoArticulos y TipoServicios

---
+++ BackEndClass/Models/TipoArticulos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEndClass.Models
+{
+    public class TipoArticulos
+    {
+        public int id { get; set; }
+        public string description { get; set; }
+        public string type { get; set; }
+        public bool status { get; set; }
+    }
+}
