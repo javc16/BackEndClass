b34bbaef097aac895addf9ecad3a353b2597defb
DESKTOP-AICR3S2\alexe <javc1603@gmail.com>
2021-05-10 22:38:41 -0600

    Adding Domain Service for UserTypeEntity

---
+++ BackEndClass/Domain/UserTypeDomainService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEndClass.Domain
+{
+    public class UserTypeDomainService
+    {
+        public string ValidateDescription(string descripcion) 
+        {
+            if (string.IsNullOrEmpty(descripcion)) 
+            {
+                return "El campo no puede estar vacio";
+            }
+
+            return "El campo fue validado con exito";
+        }
+    }
+}
---
+++ BackEndClass/Models/Recomendation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEndClass.Models
+{
+    public class Recomendation
+    {
+        public int Id { get; set; }
+        public int Stars { get; set; }
+    }
+}
