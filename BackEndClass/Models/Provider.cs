d6299b0278445288326f5e5a63f6bdda665682b6
dielc25 <65786728+dielc25@users.noreply.github.com>
2021-05-10 22:16:55 -0600

    Update Provider.cs

--- BackEndClass/Models/Provider.cs
+++ BackEndClass/Models/Provider.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace MechanicalWorkShopSystem.Models
+namespace BackEndClass.Models
 {
     public class Provider
     {
