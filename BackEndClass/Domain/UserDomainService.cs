5c19a55f9a4b931178678dea95ae988e90cf8d59
JONATHAN AGUILAR <jjaguilar@unitec.edu>
2021-05-11 07:58:39 -0600

    Entidad y Dominio para User

---
+++ BackEndClass/Domain/UserDomainService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEndClass.Domain
+{
+    public class UserDomainService
+    {
+        public string ValidateFirstName(string firstName)
+        {
+            if (string.IsNullOrEmpty(firstName))
+            {
+                return "El campo no puede estar vacio";
+            }
+
+            return "El campo fue validado con exito";
+        }
+
+        public string ValidateLastName(string lastName)
+        {
+            if (string.IsNullOrEmpty(lastName))
+            {
+                return "El campo no puede estar vacio";
+            }
+
+            return "El campo fue validado con exito";
+        }
+    }
+}
---
+++ BackEndClass/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEndClass.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public int UserTypeId { get; set; }
+        public string Status { get; set; }
+
+
+    }
+}
