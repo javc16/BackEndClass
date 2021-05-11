eb76ae9afd6e198fa556491f6870fb7eb3ccb346
jxquinonez <77907758+jxquinonez@users.noreply.github.com>
2021-05-10 19:14:11 -0600

    Add Services entity

---
+++ BackEndClass/Models/Servicios.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEndClass.Models
+{
+    public class Servicios
+    {
+        public int id { get; set; }
+        public string description { get; set; }
+        public string alias { get; set; }
+        public int TipoServiciosID { get; set; }
+        public int price { get; set; }
+        public string status { get; set; }
+        public string name { get; set; }
+    }
+}
