07a38d7772e089331fcdf1dd2ac68d330db207ca
javychavez10 <45776782+javychavez10@users.noreply.github.com>
2021-05-12 23:48:27 -0600

    Estandarizacion
    Corrigiendo nombre de variables

--- BackEndClass/AppServices/ITArticuloAppService.cs
+++ BackEndClass/AppServices/ITArticuloAppService.cs
@@ -10,6 +10,7 @@ namespace BackEndClass.AppServices
     interface ArticuloAppServices
     {
         IEnumerable<Articulo> GetAll();
+        Task<Response> GetById(long id);
         Task<Response> PostArticulo(Articulo Articulo);
         Task<Response> PutArticulo(Articulo Articulo);
         Task<Response> DeleteArticuloUsuario(int id);
