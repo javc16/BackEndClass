481758c2c7915311b861cc16d280bfc5b5b0b574
DESKTOP-AICR3S2\alexe <javc1603@gmail.com>
2021-05-14 19:10:09 -0600

    Agregando el controlador de TipoUsuario

---
+++ BackEndClass/Controllers/TipoUsuarioController.cs
@@ -0,0 +1,57 @@
+﻿using BackEndClass.AppServices;
+using BackEndClass.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEndClass.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TipoUsuarioController : ControllerBase
+    {
+        private readonly ITipoUsuarioAppService _tipoUsuarioAppService;
+
+        public TipoUsuarioController(ITipoUsuarioAppService tipoUsuarioAppService)
+        {
+            _tipoUsuarioAppService = tipoUsuarioAppService;
+        }
+
+
+        [HttpGet]
+        public  ActionResult<IEnumerable<TipoUsuario>> GetAll()
+        {
+            var result = _tipoUsuarioAppService.GetAll();
+            return Ok(result);
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TipoUsuario>> GetById(long id)
+        {
+            return Ok(await _tipoUsuarioAppService.GetById(id));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<TipoUsuario>> Post(TipoUsuario item)
+        {
+            return Ok(await _tipoUsuarioAppService.PostTipoUsuario(item));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<TipoUsuario>> PutCiudadano(TipoUsuario item)
+        {
+            return Ok(await _tipoUsuarioAppService.PutTipoUsuario(item));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<TipoUsuario>> DeleteById(int id)
+        {
+            return Ok(await _tipoUsuarioAppService.DeleteTipoUsuario(id));
+        }
+
+    }
+}
