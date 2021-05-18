9533aa0345254ed0ea24d0f1ff642cf6a46bc3d6
jxquinonez <77907758+jxquinonez@users.noreply.github.com>
2021-05-17 21:08:43 -0600

    Add ServicioDTO

---
+++ BackEndClass/Models/DTOs/ServicioDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEndClass.Models
+{
+    public class Servicio
+    {
+        public int id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string Alias { get; set; }
+        public decimal Precio { get; set; }
+        public int Estado { get; set; }
+    }
+}
