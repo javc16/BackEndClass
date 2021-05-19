7e4b67ade838481ba208d6374289f99c46612409
javychavez10 <45776782+javychavez10@users.noreply.github.com>
2021-05-18 19:13:39 -0600

    Builder Partner
    Se añadio Builder Partner a la entidad

--- BackEndClass/Models/Articulo.cs
+++ BackEndClass/Models/Articulo.cs
@@ -14,5 +14,40 @@ namespace BackEndClass.Models
         public string Alias { get; set; }
         public decimal Precio { get; set; }
         public int Estado { get; set; }
+
+        public sealed class Builder 
+        {
+            private readonly Articulo _articulo;
+
+            public Builder(int id ) 
+            {
+                _articulo = new Articulo
+                {
+                    Id = id
+                };   
+            }
+
+            public Builder conNombre(string nombre) 
+            {
+                _articulo.Nombre = nombre;
+                return this;
+            }
+            public Builder conDescripcion(string descripcion) 
+            {
+                _articulo.Descripcion = descripcion;
+                return this;
+            }
+
+            public Builder conPrecio(decimal precio) 
+            {
+                _articulo.Precio = precio;
+                return this;
+            }
+            public Articulo Constuir() 
+            {
+                return _articulo;
+            }
+        }
+
     }
 }
--- BackEndClass/Models/DTOs/ArticuloDTO.cs
+++ BackEndClass/Models/DTOs/ArticuloDTO.cs
@@ -14,5 +14,43 @@ namespace BackEndClass.Models
         public string Alias { get; set; }
         public decimal Precio { get; set; }
         public int Estado { get; set; }
+
+        public static ArticuloDTO DeModeloADTO(Articulo articulo) 
+        {
+            return articulo != null ? new ArticuloDTO 
+            {
+                Id = articulo.Id,
+                Nombre = articulo.Nombre,
+                Descripcion = articulo.Descripcion,
+                Precio = articulo.Precio
+            }:null;
+        }
+
+        public static IEnumerable<ArticuloDTO> DeModeloADTO(IEnumerable<Articulo> articulo)
+        {
+            if (articulo == null) 
+            {
+                return new List<ArticuloDTO>();
+            }
+            List<ArticuloDTO> articuloData = new List<ArticuloDTO>();
+
+            foreach (var item in articulo) 
+            {
+                articuloData.Add(DeModeloADTO(item));
+            }
+
+            return articuloData;
+        }
+
+
+        public static Articulo DeDTOAModelo(Articulo articuloDTO)
+        {
+            return articuloDTO != null ? new Articulo.Builder(articuloDTO.Id).conNombre(articuloDTO.Nombre).conDescripcion(articuloDTO.Descripcion).conPrecio(articuloDTO.Precio).Constuir() : null;
+        }
     }
+
+    
 }
+
+    
+
