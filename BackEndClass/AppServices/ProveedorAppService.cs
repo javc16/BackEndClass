d217e44626d29a586d39df01a51fe6cd3842f7be
DESKTOP-AICR3S2\alexe <javc1603@gmail.com>
2021-06-17 22:19:01 -0600

    editando metodo de guardar proveedor

--- BackEndClass/AppServices/ProveedorAppService.cs
+++ BackEndClass/AppServices/ProveedorAppService.cs
@@ -57,10 +57,12 @@ namespace BackEndClass.AppServices
             }
             else if ((GuardarProveedor != null && GuardarProveedor.Estado == Constantes.Inactivo)) 
             {
-                GuardarProveedor.Estado = Constantes.Activo;
-                _context.Entry(GuardarProveedor).State = EntityState.Modified;
+
+                Proveedor proveedorEditado =ProveedorDTO.DeDTOAModelo(proveedorDTO);
+                proveedorEditado.Id = GuardarProveedor.Id;
+                _context.Entry(proveedorEditado).State = EntityState.Modified;
                 await _context.SaveChangesAsync();
-                return new Response { Mensaje = $"Proveedor {GuardarProveedor.Nombre} activado correctamente" };
+                return new Response { Mensaje = $"Proveedor {proveedorEditado.Nombre} activado correctamente" };
             }
             var proveedor = ProveedorDTO.DeDTOAModelo(proveedorDTO);
             _context.Proveedor.Add(proveedor);
